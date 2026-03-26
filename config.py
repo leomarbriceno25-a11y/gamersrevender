8053964fc6824d8ebdacd146e3280afe3c420f8d
Leomar <leomarbriceno25@gmail.com>
2026-03-26 18:32:26 -0500

    Integrate Razer recharge API with package 1-10 support

--- config.py
+++ config.py
@@ -23,6 +23,12 @@ WHATSAPP_NUMERO = '521234567890'
 # API Hype Games
 HYPE_API_URL = 'https://redeem.hype.games'
 
+# API Razer Recargas
+RAZER_API_URL = os.environ.get('RAZER_API_URL', 'https://152.53.210.85/api/razer.jsp')
+RAZER_API_TOKEN = os.environ.get('RAZER_API_TOKEN', 'FOJXLF2DCUCNBE4OJFONBE4ODEM23YGJLTKWSFONBE4OJXL')
+# El endpoint actual usa certificado no confiable; poner "1" para forzar validación cuando corrijan SSL.
+RAZER_VERIFY_SSL = os.environ.get('RAZER_VERIFY_SSL', '0') == '1'
+
 # GamePoint Club API
 GAMEPOINT_PARTNER_ID = os.environ.get('GAMEPOINT_PARTNER_ID', '8e444f41-3c46-4226-b90d-2fb8db46fbed')
 GAMEPOINT_SECRET_KEY = os.environ.get('GAMEPOINT_SECRET_KEY', '4d642026a78b03d66710')
