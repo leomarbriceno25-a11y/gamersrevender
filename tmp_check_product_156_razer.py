09ba3a7863a9177de5c79211ec3a3d35e1558445
Leomar <leomarbriceno25@gmail.com>
2026-07-23 13:01:04 -0500

    chore: elimina archivos temporales de debug y los ignora en git

--- tmp_check_product_156_razer.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-con = sqlite3.connect('/var/www/tienda/instance/tienda.db')
-con.row_factory = sqlite3.Row
-cur = con.cursor()
-r = cur.execute("SELECT id,nombre,usa_razer,razer_paquete,usa_api,activo FROM productos WHERE id=156").fetchone()
-print(dict(r) if r else None)
-con.close()
